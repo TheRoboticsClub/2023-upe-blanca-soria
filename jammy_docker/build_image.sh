3308214b3042cc031cadca172e9672a91b565ddc
Blancasr <b.soaria.2019@alumnos.urjc.es>
2023-02-20 14:21:34 +0100

    added gazebo in Dockerfile.base
    Signed-off-by: Blancasr <b.soaria.2019@alumnos.urjc.es>

--- jammy_docker/build_image.sh
+++ jammy_docker/build_image.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [ $# -gt 1 ] || [ $# -eq 0 ] ; then
+    name=turtlebot2
+else
+    name=$1
+fi
+
 docker build -f Dockerfile.base -t humble_base .
-docker build -f Dockerfile.turtlebot --no-cache=true -t  turtlebot2 .
+docker build -f Dockerfile.turtlebot --no-cache=true -t $name .
